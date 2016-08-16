64c6f0284b4bffb7bb10d726eada4381953b93a4
chris martinez and Mark Lawson <chris@>
2016-08-16 11:52:11 -0700

    separate business and and client side logic

--- js/prime-sifting.js
+++ js/prime-sifting.js
@@ -1,24 +1,28 @@
-$(function() {
-  $("#submit").click(function(event) {
-    var num = parseInt($('#number').val());
-    var allNumbers = [];
 
-
-    for(var i = 2; i<=num; i++){
-      allNumbers.push(i);
-    }
-
-    for (var p = 2; p <num; p++){
-      if(allNumbers.indexOf(p) >= 0){
-        for (var j=0; j<allNumbers.length; j++) {
-          for (var i= p; i<=num; i+= p ){
-            if (allNumbers[j] === i && i > p) {
-              allNumbers.splice(j, 1);
-            }
+function primeNumber(number, array) {
+  for (var p = 2; p <number; p++){
+    if(array.indexOf(p) >= 0){
+      for (var j=0; j<array.length; j++) {
+        for (var i= p; i<=number; i+= p ){
+          if (array[j] === i && i > p) {
+            array.splice(j, 1);
           }
         }
       }
     }
+  }
+}
+
+
+$(function() {
+  $("#submit").click(function(event) {
+    var num = parseInt($('#number').val());
+    var allNumbers = [];
+    for(var i = 2; i<=num; i++){
+      allNumbers.push(i);
+    }
+    primeNumber(num, allNumbers);
+
     for (var i = 0; i<allNumbers.length; i++ ){
       $("#results").append(allNumbers[i]+" ");
     }
